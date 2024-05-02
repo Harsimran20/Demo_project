f0ad358d51d85153e21e6d97c1f9e3fe903b2807
Harsimran20 <harsimran9095@gmail.com>
2024-05-02 22:42:15 +0530

    Message

---
+++ Interactive.js
@@ -0,0 +1,12 @@
+let element=document.querySelector("button");
+function turn(){
+    element.style.backgroundColor="black";
+    element.style.color="White";
+    document.getElementById.innerHTML="Thank you for submitting";
+};
+/*function back(){
+    element.style.backgroundColor="red";
+    element.style.color="White";
+};*/
+element.onclick=turn();
+//element.onclick=back();
